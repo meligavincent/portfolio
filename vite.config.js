79d141d001f93e5280a7345ef6af2923a75c1caa
Senbonsakurakeyoshi1 <699580621meliga@gmail.com>
2024-03-26 18:42:16 +0100

    pain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Portofolio3D/",
+  base:"/portofolio/",
   plugins: [react()],
   build: {
     outDir: 'build', // Spécifie le nom du dossier de sortie
